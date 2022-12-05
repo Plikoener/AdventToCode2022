318bb272370a6e336ccac5f8c88fc177cc3337ed
Plikoener <dev@plikoener.de>
2022-12-05 22:55:48 +0100

    optimize import by IDE

--- test/de/pliconer/utils/ConvertUtilsTest.java
+++ test/de/pliconer/utils/ConvertUtilsTest.java
@@ -3,7 +3,8 @@ package de.pliconer.utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConvertUtilsTest {
 
