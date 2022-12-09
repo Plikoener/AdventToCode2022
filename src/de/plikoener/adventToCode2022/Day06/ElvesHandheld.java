7f168b3817706afd20f25078fb9ddadd425d7109
Plikoener <rp@rene-plitzko.de>
2022-12-09 12:14:21 +0100

    add ElvesHandheld methods isMarker and findPositionOfMarker

--- src/de/plikoener/adventToCode2022/Day06/ElvesHandheld.java
+++ src/de/plikoener/adventToCode2022/Day06/ElvesHandheld.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class ElvesHandheld extends AdventToCodeInputLoader {
     private static final int MARKERS_LENGTH = 4;
      ArrayDeque<Character> markerQueue = new ArrayDeque<>(MARKERS_LENGTH);
-
+    //TODO change to fixed size queue see https://www.baeldung.com/java-fixed-size-queue
     final static boolean DEBUG = true;
     public ElvesHandheld(String fileName) throws IOException {
         super(fileName);
@@ -27,28 +27,69 @@ public class ElvesHandheld extends AdventToCodeInputLoader {
     }
 
     public  int findPositionOfMarker(String input) {
+        //TODO check if using String.substring is easier an isMarker(String markerCandidate,int markerSize)
         char[] inputAsArray= input.toCharArray();
+        //markerQueue.clear();
         for(int charRead=0;charRead<inputAsArray.length;charRead++)
         {
+
             markerQueue.push(inputAsArray[charRead]);
             if (markerQueue.size()== MARKERS_LENGTH) {
-//                Object[] objArray = markerQueue.toArray();
-//                Character[] markerCandidate = new Character[objArray.length];
-//                System.arraycopy(objArray,0,markerCandidate,0,objArray.length);
-//                for (Character markerCharacter : markerCandidate) {
-//                }
-                Iterator<Character> markerChar = markerQueue.iterator();
-                while (markerChar.hasNext())
+                if (isMarker(convertArrayQueueToStack(markerQueue)))
                 {
-                    markerChar.next();
-                    if (false)
-                        return charRead;
-
+                    markerQueue.clear();
+                    return charRead+1;
                 }
-// TODO Implement the check if the queue has a marker , extract to extra method
+                markerQueue.removeLast();
+            }
+
+        }
+        return -1;
+    }
+    public int findPositionOfMarker2(String input)
+    {
+        for (int inputCounter=0;inputCounter<input.length()-MARKERS_LENGTH;inputCounter++)
+        {
+            if(isMarker(input.substring(inputCounter,inputCounter-MARKERS_LENGTH),MARKERS_LENGTH))
+            {
+                return  inputCounter+1;
             }
         }
         return -1;
+    }
+
+    private Stack<Character> convertArrayQueueToStack(ArrayDeque<Character> queue) {
+        List list = Arrays.asList(queue);
+        Stack<Character> markerCandidate =  new Stack<>();
+        for (Character character : queue) {
+            markerCandidate.push(character);
+        }
+        return markerCandidate;
+    }
+    public static boolean isMarker(String markerCandidate, int markerLength)
+    {
+        //TODO implement
+        Stack<Character> stack = new Stack<>();
+        for (char sign:markerCandidate.toCharArray()
+             ) {
+            stack.push(Character.valueOf(sign));
+
+        }
+        return false;
+    }
+
+    public static boolean isMarker(Stack<Character> markerCandidate) {
+        while(!markerCandidate.isEmpty())
+        {
+            Character character = markerCandidate.pop();
+            if(markerCandidate.contains(character))
+            {
+             return false;
+            }
+
+
+        }
+        return true;
     }
 
     String getFirstMessage()
--- test/de/plikoener/adventToCode2022/Day06/ElvesHandheldTest.java
+++ test/de/plikoener/adventToCode2022/Day06/ElvesHandheldTest.java
@@ -1,12 +1,22 @@
 package de.plikoener.adventToCode2022.Day06;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ElvesHandheldTest {
+
+    private ElvesHandheld elvesHandheld;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        elvesHandheld = new ElvesHandheld("");
+    }
+
     @Test
     void ConstructorTest() throws IOException {
         String message1 = "ABCDE";
@@ -20,5 +30,46 @@ class ElvesHandheldTest {
         String input1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
         ElvesHandheld elvesHandheld = new ElvesHandheld("");
         assertEquals(7,elvesHandheld.findPositionOfMarker(input1));
+    }
+
+    @Test
+    void isMarker() {
+        String[][]testDataArray ={
+            {"true","abc"},
+            {"false","abbc"},
+            {"false","abcc"},
+            {"false","aabc"}
+             
+        };
+        for (String[] testData:testDataArray
+             ) {
+            Stack<Character> testStack = new Stack<>();
+            for (char testC:testData[1].toCharArray()
+                 ) {
+                testStack.push(Character.valueOf(testC));
+            }
+            assertEquals(Boolean.valueOf(testData[0]),ElvesHandheld.isMarker(testStack));
+        }
+
+    }
+
+    @Test
+    void testFindPositionOfMarker() throws IOException {
+        assertEquals(7, elvesHandheld.findPositionOfMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assertEquals(5, elvesHandheld.findPositionOfMarker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assertEquals(6, elvesHandheld.findPositionOfMarker("nppdvjthqldpwncqszvftbrmjlhg"));
+        assertEquals(10, elvesHandheld.findPositionOfMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assertEquals(11, elvesHandheld.findPositionOfMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+
+    }
+
+    @Test
+    void testFindPositionOfMarker1() {
+        //TODO implement this test with example from desciption
+    }
+
+    @Test
+    void testIsMarker() {
+        assertFalse(ElvesHandheld.isMarker("aabb",4));
     }
 }
