c2543d8a1329e9983c47e3579b75506778826256
Plikoener <rp@rene-plitzko.de>
2023-01-02 20:52:51 +0100

    add GFG findSum with modification to use Arrays with different length

--- src/org/geeksforgeeks/findOverlappingSumOfTwoArrays/GFG.java
+++ src/org/geeksforgeeks/findOverlappingSumOfTwoArrays/GFG.java
@@ -13,12 +13,12 @@ public class GFG
      * overlapping sum of two array
      * @param A first array
      * @param B secound array
-     * @param n sum of overlapped entries
-     * @return
+     * @param n length of one sum - not longer used
+     * @return sum of overlapped entries
      */
 //
     //
-    public static int findSum(int A[], int B[], int n)
+    public static int findSum(int[] A, int[] B, @Deprecated int n)
     {
 
         // unordered map to store count of 
@@ -27,29 +27,21 @@ public class GFG
 
         // insert elements of A[] into
         // unordered_map
-        for(int i = 0; i < n; i++)
-        {
-            if(!hash.containsKey(A[i]))
-            {
-                hash.put(A[i], 1);
-            }
-            else
-            {
-                hash.put(A[i], hash.get(A[i]) + 1);
+        for (int j : A) {
+            if (!hash.containsKey(j)) {
+                hash.put(j, 1);
+            } else {
+                hash.put(j, hash.get(j) + 1);
             }
         }
 
         // insert elements of B[] into
         // unordered_map
-        for(int i = 0; i < n; i++)
-        {
-            if(!hash.containsKey(B[i]))
-            {
-                hash.put(B[i], 1);
-            }
-            else
-            {
-                hash.put(B[i], hash.get(B[i]) + 1);
+        for (int j : B) {
+            if (!hash.containsKey(j)) {
+                hash.put(j, 1);
+            } else {
+                hash.put(j, hash.get(j) + 1);
             }
         }
 
@@ -68,8 +60,8 @@ public class GFG
     // Driver code
     public static void main (String[] args)
     {
-        int A[] = { 5, 4, 9, 2, 3 };
-        int B[] = { 2, 8, 7, 6, 3 };
+        int[] A = { 5, 4, 9, 2, 3 };
+        int[] B = { 2, 8, 7, 6, 3 };
 
         // size of array
         int n = A.length;
